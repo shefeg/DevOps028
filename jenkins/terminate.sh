3e8c1a01059d1971df846c0b83d2ef4f2cd295f6
Alexey Ignatenko <a.ignatenko1988@gmail.com>
2017-10-10 15:32:33 +0300

    fixes

---
+++ jenkins/terminate.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+for i in `/usr/bin/aws ec2 describe-instances --filters \"Name=tag-value,Values=${params.INSTANCE_NAME}\" | grep \"InstanceId\" | cut -d':' -f2 | tr -d '\", '`; do aws ec2 terminate-instances --instance-ids $i; done;
