2426fadf57fb968cc0d7ab7693366e9f050393ec
Alexey Ignatenko <a.ignatenko1988@gmail.com>
2017-10-31 23:17:49 +0200

    deployment fixes

--- jenkins/prepare_key.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/usr/bin/aws ssm get-parameters --names "aipk" | grep "Value" | cut -d':' -f2 | tr -d '",' > aipk.pem
-sed -i 's/\\n/\n/g' aipk.pem && sed -i '1s/ //' aipk.pem
-chmod 600 aipk.pem
-echo "SSH key prepared for using"
